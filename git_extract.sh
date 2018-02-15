01eb4d1b568801b37e2d6129856fd0a8c33153db
Szymon Balicki <szymonb0@gmail.com>
2018-02-15 11:31:49 +0100

    Update and rename git_history_keeper.sh to git_extract.sh

--- git_extract.sh
+++ git_extract.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-echo "This script must be run under root directory of your repository"
-echo "Please specify the directory which should keep its history. Path to this directory should be relative to git root directory."
+echo "This script must be run from root directory of your repository"
+echo "Please specify the subdirectory which should be extracted. Path to this subdirectory should be relative to git root directory."
 read KEEP_DIR_PATH
 
-TMP_DIR=`pwd`"/tmp"
+TMP_DIR="/tmp"
 FILES_TO_KEEP="$TMP_DIR/files_to_keep"
 FILES_TO_REMOVE="$TMP_DIR/files_to_remove"
 HELPER_SCRIPT="$TMP_DIR/helper.sh"
@@ -19,16 +19,16 @@ touch "$FILES_TO_KEEP"
 find "$KEEP_DIR_PATH" -exec "$HELPER_SCRIPT" '{}' \;
 sort -u "$FILES_TO_KEEP" -o "$FILES_TO_KEEP"
 
-# move files to newroot dir - modified command from git filter-branch help examples
+# move files to newsubdir - modified command from git filter-branch help examples
 export FILES_TO_KEEP
 git filter-branch --index-filter \
-    'git ls-files -s | grep -wFf "$FILES_TO_KEEP" | sed "s-\t\"*-&newroot/-" |
+    'git ls-files -s | grep -wFf "$FILES_TO_KEEP" | sed "s-\t\"*-&newsubdir/-" |
 		GIT_INDEX_FILE=$GIT_INDEX_FILE.new \
 			git update-index --index-info &&
 	 mv "$GIT_INDEX_FILE.new" "$GIT_INDEX_FILE" || true' HEAD
 
 # extract the new folder to the root of the repo
-git filter-branch --prune-empty -f --subdirectory-filter newroot
+git filter-branch --prune-empty -f --subdirectory-filter newsubdir
 
 # remove other files left accidentally
 touch "$FILES_TO_REMOVE"
@@ -41,6 +41,3 @@ git filter-branch --prune-empty -f --tree-filter \
 	fi
     done < "$FILES_TO_REMOVE"'
 git gc --aggressive
-
-# clean up temporary files
-trap "{ rm -rf $TMP_DIR; }" EXIT
